8039cb4ad7ae686c8fd3c9da39a731b7e439d81a
goature <1069929856@qq.com>
2019-12-23 15:44:24 +0800

    测试

---
+++ src/main/java/com/aaa/demo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.aaa.demo.controller;
+
+/**
+ * Description:
+ * Author:         goature
+ * CreateDate:     2019/12/23 0023 15:37
+ * UpdateUser:     goature
+ * UpdateRemark:   修改内容
+ * Version:        1.0
+ */
+public class TestController {
+}
